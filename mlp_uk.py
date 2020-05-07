28cd37745a51c5db4c404359d9a44f721e60f5b2
lamharrison <lamharrison123@gmail.com>
2020-05-07 20:46:07 +0100

    update data

--- mlp_uk.py
+++ mlp_uk.py
@@ -39,7 +39,7 @@ italy_absorb = corn_y[italy_dates_length-1]*1.3
 
 corn_y_norm = corn_y / italy_absorb
 
-model.fit(corn_x, corn_y_norm, epochs=25000, shuffle=False)
+model.fit(corn_x, corn_y_norm, epochs=20000, shuffle=False)
 corn_y_predict = model.predict(corn_x)
 corn_y_predict = corn_y_predict * italy_absorb
 fig1 = plt.figure(figsize=(7, 5))
@@ -75,7 +75,7 @@ ger_absorb = ger_corn_y[ger_dates_length-1]*1.3
 
 corn_y_norm = ger_corn_y / ger_absorb
 
-model.fit(ger_corn_x, corn_y_norm, epochs=25000, shuffle=False)
+model.fit(ger_corn_x, corn_y_norm, epochs=20000, shuffle=False)
 corn_y_predict = model.predict(ger_corn_x)
 corn_y_predict = corn_y_predict * ger_absorb
 fig_italy = plt.figure(figsize=(7, 5))
@@ -111,7 +111,7 @@ fr_absorb = fr_corn_y[fr_dates_length-1]*1.3
 
 corn_y_norm = fr_corn_y / fr_absorb
 
-model.fit(fr_corn_x, corn_y_norm, epochs=27000, shuffle=False)
+model.fit(fr_corn_x, corn_y_norm, epochs=20000, shuffle=False)
 corn_y_predict = model.predict(fr_corn_x)
 corn_y_predict = corn_y_predict * fr_absorb
 fig_italy = plt.figure(figsize=(7, 5))
@@ -145,7 +145,7 @@ for each in each_data:
     uk_comfirmed_data.append(each['confirmed'])
 
 # add uk latest data manually
-uk_comfirmed_data.append(120064)
+uk_comfirmed_data.append(206715)
 
 uk_date_length = len(uk_comfirmed_data)
 uk_dates = list(range(1, uk_date_length + 1))
@@ -154,12 +154,12 @@ uk_comfirmed_data = np.array(uk_comfirmed_data)
 uk_dates = np.array(uk_dates)
 
 # increase absorb value
-uk_absorb_amount = uk_comfirmed_data[uk_date_length-1]*2
+uk_absorb_amount = uk_comfirmed_data[uk_date_length-1]*1.75
 
 uk_comfirmed_data_norm = uk_comfirmed_data / uk_absorb_amount
 
 # fit model
-model.fit(uk_dates, uk_comfirmed_data_norm, epochs=27000, shuffle=False)
+model.fit(uk_dates, uk_comfirmed_data_norm, epochs=30000, shuffle=False)
 
 uk_comfirmed_data_predict = model.predict(uk_dates)
 uk_comfirmed_data_predict = uk_comfirmed_data_predict * uk_absorb_amount
@@ -172,7 +172,7 @@ plt.ylabel('Amount')
 plt.legend()
 plt.show()
 
-uk_comfirmed_data_predict = np.array(list(range(1, uk_date_length+8)))
+uk_comfirmed_data_predict = np.array(list(range(1, uk_date_length+31)))
 uk_comfirmed_predict_7_days = model.predict(uk_comfirmed_data_predict)
 fig3 = plt.figure(figsize=(7, 5))
 plt.scatter(uk_dates, uk_comfirmed_data, label='Real Confirmed')
@@ -186,5 +186,5 @@ plt.show()
 
 # save prediction data
 with open('uk_prediction_data/uk_prediction.json', 'w') as f:
-    dict_uk_data = dict(zip(list(range(1, uk_date_length+8)), (uk_comfirmed_predict_7_days*uk_absorb_amount).tolist()))
+    dict_uk_data = dict(zip(list(range(1, uk_date_length+31)), (uk_comfirmed_predict_7_days*uk_absorb_amount).tolist()))
     json.dump(dict_uk_data, f)
--- mlp_uk_learning.py
+++ mlp_uk_learning.py
@@ -1,9 +1,9 @@
 import numpy as np
+
 np.random.seed(1337)
 from keras.models import Sequential
 from keras.layers import Dense
 import matplotlib.pyplot as plt
-
 
 model = Sequential()
 model.add(Dense(units=50, input_dim=1, activation='relu'))
@@ -34,18 +34,22 @@ uk_comfirmed_data = []
 for each in each_data:
     uk_comfirmed_data.append(each['confirmed'])
 
+# add uk latest data manually
+#uk_comfirmed_data.append(161145)
+
 uk_date_length = len(uk_comfirmed_data)
 uk_dates = list(range(1, uk_date_length + 1))
 
 uk_comfirmed_data = np.array(uk_comfirmed_data)
 uk_dates = np.array(uk_dates)
 
-uk_absorb_amount = uk_comfirmed_data[uk_date_length-1]
+# increase absorb value
+uk_absorb_amount = uk_comfirmed_data[uk_date_length - 1] * 7
 
 uk_comfirmed_data_norm = uk_comfirmed_data / uk_absorb_amount
 
 # fit model
-model.fit(uk_dates, uk_comfirmed_data_norm, epochs=10000, shuffle=False)
+model.fit(uk_dates, uk_comfirmed_data_norm, epochs=30000, shuffle=False)
 
 uk_comfirmed_data_predict = model.predict(uk_dates)
 uk_comfirmed_data_predict = uk_comfirmed_data_predict * uk_absorb_amount
@@ -57,3 +61,21 @@ plt.xlabel('Dates')
 plt.ylabel('Amount')
 plt.legend()
 plt.show()
+
+uk_comfirmed_data_predict = np.array(list(range(1, uk_date_length + 31)))
+uk_comfirmed_predict_7_days = model.predict(uk_comfirmed_data_predict)
+fig3 = plt.figure(figsize=(7, 5))
+plt.scatter(uk_dates, uk_comfirmed_data, label='Real Confirmed')
+plt.plot(uk_comfirmed_data_predict, uk_comfirmed_predict_7_days * uk_absorb_amount, label='Predict Result')
+plt.title('UK Prediction Confirmed VS Dates')
+plt.xlabel('Dates')
+plt.ylabel('Amount')
+plt.legend()
+plt.savefig('uk_model_7_days.png')
+plt.show()
+
+# save prediction data
+with open('uk_prediction_data/uk_prediction.json', 'w') as f:
+    dict_uk_data = dict(
+        zip(list(range(1, uk_date_length + 31)), (uk_comfirmed_predict_7_days * uk_absorb_amount).tolist()))
+    json.dump(dict_uk_data, f)
